6203721cc30c83ebd50d24be89c380fc1fd23b41
Ruben David <ruben@jobs2live.com>
2024-04-28 10:38:11 -0400

    Sec-8 Fernando Herrera 06 Creando los módulos y los componentes básicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { SharedModule } from "./shared/shared.module";
+import { PaisModule } from './pais/pais.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule,SharedModule,PaisModule],
     providers: [],
     bootstrap:[AppComponent]
 })
