1640a0c66a2b9cfe3db2728bed2cb4a69b420ba3
Ruben David <ruben@jobs2live.com>
2024-04-30 15:40:47 -0400

    Fernando Herrera Sec-8 021 Ver país de forma independiente

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
 })
-export class VerPaisComponent {
+export class VerPaisComponent implements OnInit {
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
+  ngOnInit(): void {
+    this.activateRoute.params.subscribe(({ id }) => {
+      this.paisService.getPaisPorAplpha(id).subscribe((pais) => {
+        console.log(pais);
+      });
+    });
+  }
 }
