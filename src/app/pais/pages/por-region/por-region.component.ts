e38742774a55722b914e70e74ef0fe375ff27ca6
Ruben David <ruben@jobs2live.com>
2024-05-01 07:50:24 -0400

    Fernando Herrera Sec-9 04 ngClass , class [class.]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -3,7 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
+  styles: [
+    `
+      button {
+        margin-right: 5px;
+      }
+    `,
+  ],
 })
 export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string = '';
+  constructor() {}
 
+  activarRegion(region: string) {
+    this.regionActiva = region;
+  }
 }
